6a119e98f8bbfd5565c88d934c7a649872c5e278
Garrett Heaver <garrett@iterationfour.com>
2013-11-19 15:22:44 +0000

    refactored layout of user model specs

--- spec/lib/models/user_spec.rb
+++ spec/lib/models/user_spec.rb
@@ -6,30 +6,38 @@ describe User do
 
     let(:user) { create(:user) }
 
-    it 'finds the user by username' do
-      found = User.by_login(user.username)
-      expect(found).to eq(user)
+    context 'when given a username' do
+      it 'finds the user' do
+        found = User.by_login(user.username)
+        expect(found).to eq(user)
+      end
     end
 
-    it 'finds the user by email' do
-      found = User.by_login(user.email)
-      expect(found).to eq(user)
+    context 'when given an email' do
+      it 'finds the user' do
+        found = User.by_login(user.email)
+        expect(found).to eq(user)
+      end
     end
 
   end
 
   describe '#password' do
 
-    it 'stores the password using bcrypt with cost 10' do
-      user0 = User.new(password: 'garrett')
-      hash0 = BCrypt::Password.new(user0.values[:password])
-      expect(hash0.cost).to eq(10)
+    context 'when setting a new value' do
+      it 'stores it using bcrypt cost 10' do
+        user0 = User.new(password: 'garrett')
+        hash0 = BCrypt::Password.new(user0.values[:password])
+        expect(hash0.cost).to eq(10)
+      end
     end
 
-    it 'returns a bcrypt password object for equality checking' do
-      hash0 = BCrypt::Password.create('garrett')
-      user0 = User.load(password: hash0.to_s)
-      expect(user0.password).to eq('garrett')
+    context 'when reading an existing value' do
+      it 'returns a bcrypt password checking object' do
+        hash0 = BCrypt::Password.create('garrett')
+        user0 = User.load(password: hash0.to_s)
+        expect(user0.password).to eq('garrett')
+      end
     end
 
   end
