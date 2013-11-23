bd52cc2a81737c82c7e74972d3d58733767566ff
Garrett Heaver <garrett@iterationfour.com>
2013-11-23 21:24:34 +0000

    updated api GET snapshots to demand correct auth right

--- app/api/v1/routes/snapshots.rb
+++ app/api/v1/routes/snapshots.rb
@@ -5,7 +5,7 @@ module Evercam
       stream = ::Stream.by_name(params[:name])
       raise NotFoundError, 'stream was not found' unless stream
 
-      unless stream.is_public? || auth.has_right?('stream:view', stream)
+      unless stream.is_public? || auth.has_right?('view', stream)
         raise AuthorizationError, 'not authorized to view this stream'
       end
 
