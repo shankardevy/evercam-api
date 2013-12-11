a5f8731ee24390f09b33270c5c9b66b302899bd7
Garrett Heaver <garrett@iterationfour.com>
2013-12-11 15:21:02 +0000

    updated user confirmation actor to expect confirmation code

--- lib/actors/user_confirm.rb
+++ lib/actors/user_confirm.rb
@@ -4,11 +4,16 @@ module Evercam
 
       required do
         string :username
+        string :confirmation
       end
 
       def validate
-        unless User.by_login(username)
+        unless user = User.by_login(username)
           add_error(:username, :exists, 'Username does not exist')
+        end
+
+        if user && user.password != confirmation
+          add_error(:confirmation, :invalid, 'Confirmation code is invalid')
         end
       end
 
--- spec/lib/actors/user_confirm_spec.rb
+++ spec/lib/actors/user_confirm_spec.rb
@@ -7,19 +7,29 @@ module Evercam
 
       subject { UserConfirm }
 
+      let(:user0) do
+        create(:user, password: 'abcd')
+      end
+
       describe 'invalid params' do
+
         it 'checks the username actually exists' do
-          outcome = subject.run(username: 'xxxx')
+          outcome = subject.run(username: 'xxxx', confirmation: 'abcd')
           expect(outcome).to_not be_success
           expect(outcome.errors.symbolic[:username]).to eq(:exists)
         end
+
+        it 'validates that the confirmation code is correct' do
+          outcome = subject.run(username: user0.username, confirmation: 'xxxx')
+          expect(outcome).to_not be_success
+          expect(outcome.errors.symbolic[:confirmation]).to eq(:invalid)
+        end
+
       end
 
       describe 'user confirmation' do
         it 'sets #confirmed_at' do
-          user0 = create(:user)
-          outcome = subject.run(username: user0.username)
-
+          outcome = subject.run(username: user0.username, confirmation: 'abcd')
           expect(outcome).to be_success
           expect(user0.reload.confirmed_at).to_not be_nil
         end
