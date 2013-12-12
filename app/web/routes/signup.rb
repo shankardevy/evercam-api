29dc4c1960d38fa3aa604d4d077d052ff25b37a5
Garrett Heaver <garrett@iterationfour.com>
2013-12-12 14:20:47 +0000

    moved POST /interested into new routes/signup file

--- app/web/routes/signup.rb
+++ app/web/routes/signup.rb
@@ -16,6 +16,21 @@ module Evercam
       end
     end
 
+    post '/interested' do
+      email = params[:email]
+
+      unless email =~ /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
+        flash[:error] = 'Sorry but the email address you entered does not appear to be valid'
+      else
+        Mailers::UserMailer.interested(email: email, request: request)
+        cookies.merge!({ email: email, created_at: Time.now.to_i })
+        flash[:success] = "Thank you for your interest. We'll be in contact soon..."
+      end
+
+      redirect '/'
+    end
+
+
   end
 end
 
--- spec/app/web/routes/signup_spec.rb
+++ spec/app/web/routes/signup_spec.rb
@@ -49,5 +49,40 @@ describe 'WebApp routes/signup' do
 
   end
 
+  describe 'POST /interested' do
+
+    context 'when the email is valid' do
+
+      it 'thanks the user for their interest' do
+        post('/interested', { email: 'garrett@evercam.io' })
+        follow_redirect!
+
+        expect(last_response.body).
+          to match(/thank you for your interest/i)
+      end
+
+      it 'creates a cookie for the email and creation date' do
+        post('/interested', { email: 'garrett@evercam.io' })
+        follow_redirect!
+
+        cookies = rack_mock_session.cookie_jar
+        expect(cookies['email']).to eq('garrett@evercam.io')
+        expect(cookies['created_at']).to_not be_nil
+      end
+
+    end
+
+    context 'when the email is invalid' do
+      it 'tells the user the address is invalid' do
+        post('/interested', { email: 'xxxx' })
+        follow_redirect!
+
+        expect(last_response.body).
+          to match(/does not appear to be valid/i)
+      end
+    end
+
+  end
+
 end
 
