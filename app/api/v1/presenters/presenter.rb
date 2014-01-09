26dc462d60b8fe0876ed84a34d341802ad2a2faa
Garrett Heaver <garrett@iterationfour.com>
2014-01-09 17:00:38 +0000

    overriding grape entity #to_json to maintain pretty printing

--- app/api/v1/presenters/presenter.rb
+++ app/api/v1/presenters/presenter.rb
@@ -3,6 +3,11 @@ module Evercam
 
     format_with(:timestamp) { |t| t.to_i }
 
+    def to_json(state)
+      opts = state.to_h if state && state.respond_to?(:to_h)
+      JSON.pretty_generate(serializable_hash(opts), state)
+    end
+
   end
 end
 
