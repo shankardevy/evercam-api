9c303821fdce2fd00cb4492423df1d5a063a0667
Michael Quinn <michael.quinn@mhlabs.net>
2014-02-05 14:59:16 +0000

    adding password reset functionality and fixing Android app links

--- app/web/app.rb
+++ app/web/app.rb
@@ -15,7 +15,8 @@
  'routes/signup_router',
  'routes/login_router',
  'routes/oauth2_router',
- 'routes/marketplace_router'
+ 'routes/marketplace_router',
+ 'routes/account_router'
 ].each { |f| require_relative "./#{f}" }
 
 module Evercam
@@ -29,6 +30,7 @@ module Evercam
     use Evercam::WebLoginRouter
     use Evercam::WebOAuth2Router
     use Evercam::WebMarketPlaceRouter
+    use Evercam::WebAccountRouter
 
   end
 end
