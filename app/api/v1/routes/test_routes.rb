f7a1a66ac76c757153bab2f25fcc3a3758e6ab91
Peter Wood <pwood@blacknorth.com>
2014-04-22 16:03:33 +0100

    Fixed an issue with the /v1/test route.

--- app/api/v1/routes/test_routes.rb
+++ app/api/v1/routes/test_routes.rb
@@ -19,12 +19,12 @@ module Evercam
 	      	query = Client.where(api_id: params[:api_id])
 	      	if query.count == 0
 	      		user = User.where(api_id: params[:api_id]).first
-	      		if user.nil? && !user.api_id.nil?
+	      		if !user.nil?
 	      		   result[:authenticated] = (user.api_key == params[:api_key])
 	      		end
 	      	else
 	      		client = query.first
-	      		result[:authenticated] = (client.secret == params[:api_key]) if !client.nil?
+	      		result[:authenticated] = (client.api_key == params[:api_key]) if !client.nil?
 	      	end
 	      	result
 	      end
