dd18e7338804449ee4e22c8e4c6227e0186cc6c4
Peter Wood <pwood@blacknorth.com>
2014-05-06 16:18:15 +0100

    Added a migration and did a library update.

--- migrations/20140506153000_drop_access_rights_unique_key.rb
+++ migrations/20140506153000_drop_access_rights_unique_key.rb
@@ -2,11 +2,13 @@ Sequel.migration do
   up do
    alter_table(:access_rights) do
       drop_index [:token_id, :camera_id, :right]
+      add_index [:token_id, :camera_id, :right], unique: false
    end
   end
 
   down do
    alter_table(:access_rights) do
+      drop_index [:token_id, :camera_id, :right]
       add_index [:token_id, :camera_id, :right], unique: true
    end
   end
