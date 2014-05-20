9b3c3f69e756598f85c896fa9c720dcfe7e12829
Tomasz Jama <gvs666@gmail.com>
2014-05-20 15:42:09 +0100

    sidekiq fix

--- scripts/sidekiq_setup.rb
+++ scripts/sidekiq_setup.rb
@@ -2,7 +2,7 @@ require 'dotenv'
 require 'sequel'
 
 Dotenv.load
-Sequel::Model.db = Sequel.connect("#{ENV['DATABASE_URL']}?pool=25")
+Sequel::Model.db = Sequel.connect("#{ENV['DATABASE_URL']}", max_connections: 25)
 
 require 'evercam_misc'
 require 'evercam_models'
