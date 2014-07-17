33653bf590285f1870e98c662ae669144f1c9252
Predrag Radovic <pradovic@itekako.com>
2014-07-17 12:45:21 +0000

    API handler for redirection to camera HLS playlist

--- app/api/v1/helpers/camera_helper.rb
+++ app/api/v1/helpers/camera_helper.rb
@@ -13,5 +13,10 @@ module Evercam
    		end
    		camera
    	end
+    def hls_url_for_camera(camera)
+	  auth = camera.config['auth']['basic']
+	  rtsp_url = "rtsp://#{auth['username']}:#{auth['password']}@#{camera.config['external_host']}:#{camera.config['external_rtsp_port']}#{camera.rtsp_url}"
+	  Evercam::Config[:hls][:base_path] + "/hls/m3u8_" + URI.escape(rtsp_url, ":/?.")
+    end
    end
 end
