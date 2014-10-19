9d03e9259752f53db08b8d54c65f1518fe9103dc
Milos Mosic <mosic.milos@gmail.com>
2014-10-20 00:37:46 +0200

    Fixed camera reporting wrong status, added async worker to Sidekiq

--- lib/workers/heartbeat_worker.rb
+++ lib/workers/heartbeat_worker.rb
@@ -98,6 +98,8 @@ module Evercam
         end
         trigger_webhook(camera)
         camera.update(updates)
+        invalidate_for_user(camera.owner.username)
+        invalidate_for_camera(camera)
         @dc.set(camera_name, camera, 0)
       rescue => e
         # we weren't expecting this (famous last words)
@@ -127,6 +129,23 @@ module Evercam
 
         hook_conn.post '', parameters.to_s
       end 
+    end
+
+    #TODO: remove this and call CacheHelper methods directly
+    def invalidate_for_user(username)
+      ['true', 'false', ''].repeated_permutation(2) do |a|
+        @dc.delete("user/cameras/#{username}/#{a[0]}/#{a[1]}")
+      end
+    end
+
+    def invalidate_for_camera(camera)
+      camera_sharees = CameraShare.where(camera_id: camera.id)
+      unless camera_sharees.blank?
+        camera_sharees.each do |user|
+          username = User[user.user_id].username
+          invalidate_for_user(username)
+        end
+      end
     end
 
   end
