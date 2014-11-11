73cf746179a7a70fd35ccf44b6ff8b3426a29fe2
Milos Mosic <mosic.milos@gmail.com>
2014-11-11 16:11:09 +0100

    Fixed missing ports being presented as zeros

--- app/api/v1/presenters/camera_presenter.rb
+++ app/api/v1/presenters/camera_presenter.rb
@@ -167,7 +167,7 @@ module Evercam
                      type: 'Integer',
                      desc: 'External http port of the camera'
                  } do |c,o|
-            c.config['external_http_port'].to_i
+            c.config['external_http_port']
           end
 
           expose :camera, documentation: {
@@ -201,7 +201,7 @@ module Evercam
                      type: 'Integer',
                      desc: 'External rtsp port of the camera'
                  } do |c,o|
-            c.config['external_rtsp_port'].to_i
+            c.config['external_rtsp_port']
           end
 
           expose :mpeg, documentation: {
@@ -245,7 +245,7 @@ module Evercam
                      type: 'Integer',
                      desc: 'Internal http port of the camera'
                  } do |c,o|
-            c.config['internal_http_port'].to_i
+            c.config['internal_http_port']
           end
 
           expose :camera, documentation: {
@@ -279,7 +279,7 @@ module Evercam
                      type: 'Integer',
                      desc: 'Internal rtsp port of the camera'
                  } do |c,o|
-            c.config['internal_rtsp_port'].to_i
+            c.config['internal_rtsp_port']
           end
 
           expose :mpeg, documentation: {
