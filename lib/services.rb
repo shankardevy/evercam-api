d81770bf2272717713bd9bd76fbb66f9f63eb29b
Milos Mosic <mosic.milos@gmail.com>
2014-11-22 12:57:27 +0100

    Changed Evercam::Services to use module accessors

--- lib/services.rb
+++ lib/services.rb
@@ -1,18 +1,16 @@
 module Evercam
   module Services
+    mattr_accessor :dalli_cache
+    mattr_accessor :s3_bucket
 
-    def self.dalli_cache
-          options = { :namespace => "app_v1", :compress => true, :expires_in => 300 }
-      if ENV["MEMCACHEDCLOUD_SERVERS"]
-        @dalli_cache = Dalli::Client.new(ENV["MEMCACHEDCLOUD_SERVERS"].split(','), :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"])
-      else
-        @dalli_cache = Dalli::Client.new('127.0.0.1:11211', options)
-      end
+    options = { :namespace => "app_v1", :compress => true, :expires_in => 300 }
+    if ENV["MEMCACHEDCLOUD_SERVERS"]
+      self.dalli_cache = Dalli::Client.new(ENV["MEMCACHEDCLOUD_SERVERS"].split(','), :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"])
+    else
+      self.dalli_cache = Dalli::Client.new('127.0.0.1:11211', options)
     end
 
-    def self.s3_bucket
-      s3 = AWS::S3.new(:access_key_id => Evercam::Config[:amazon][:access_key_id], :secret_access_key => Evercam::Config[:amazon][:secret_access_key])
-      @s3_bucket = s3.buckets['evercam-camera-assets']
-    end
+    s3 = AWS::S3.new(:access_key_id => Evercam::Config[:amazon][:access_key_id], :secret_access_key => Evercam::Config[:amazon][:secret_access_key])
+    self.s3_bucket = s3.buckets['evercam-camera-assets']
   end
 end
