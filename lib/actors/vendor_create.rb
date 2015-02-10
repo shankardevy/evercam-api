2dcc9eed26309a2bbe3768e0a66da9c480b91f59
Azhar Malik <azharmalik02@gmail.com>
2015-02-10 14:33:00 +0500

    Added Mac address validation

--- lib/actors/vendor_create.rb
+++ lib/actors/vendor_create.rb
@@ -18,7 +18,7 @@ module Evercam
 
         unless known_macs.blank?
           known_macs.each do |resource|
-            if resource && !(resource =~ /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/)
+            if resource && !(resource =~ /^([0-9A-Fa-f]{2}[:-]){2}([0-9A-Fa-f]{2})$/)
               add_error(resource, :valid, 'Mac address is invalid')
             end
           end
