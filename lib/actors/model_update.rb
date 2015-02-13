2771382374ce95eb14d8ff57a3d65de007742770
Azhar Malik <azharmalik02@gmail.com>
2015-02-13 16:17:39 +0500

    Fixed wrong parameter bugs

--- lib/actors/model_update.rb
+++ lib/actors/model_update.rb
@@ -28,22 +28,22 @@ module Evercam
         model = ::VendorModel.where(exid: inputs[:id]).first
 
         model.name = name if name
-        # [:jpg, :mjpg, :mpeg4, :mobile, :h264, :lowres].each do |resource|
-        #   url_name = "#{resource}_url"
-        #   unless inputs[url_name].blank?
-        #     inputs[url_name].prepend('/') if inputs[url_name][0,1] != '/'
-        #     if model.values[:config].has_key?('snapshots')
-        #       model.values[:config]['snapshots'].merge!({resource => inputs[url_name]})
-        #     else
-        #       model.values[:config].merge!({'snapshots' => { resource => inputs[url_name]}})
-        #     end
-        #   end
-        # end
+        [:jpg, :mjpg, :mpeg4, :mobile, :h264, :lowres].each do |resource|
+          url_name = "#{resource}_url"
+          unless inputs[url_name].blank?
+            inputs[url_name].prepend('/') if inputs[url_name][0,1] != '/'
+            if model.values[:config].has_key?('snapshots')
+              model.values[:config]['snapshots'].merge!({resource => inputs[url_name]})
+            else
+              model.values[:config].merge!({'snapshots' => { resource => inputs[url_name]}})
+            end
+          end
+        end
 
-        # if inputs[:default_username] or inputs[:default_password]
-        #   model.values[:config].merge!({'auth' => {'basic' => {'username' => inputs[:default_username].to_s.empty? ? '' : inputs[:default_username],
-        #                                                         'password' => inputs[:default_password].to_s.empty? ? '' : inputs[:default_password]}}})
-        # end
+        if inputs[:default_username] or inputs[:default_password]
+          model.values[:config].merge!({'auth' => {'basic' => {'username' => inputs[:default_username].to_s.empty? ? '' : inputs[:default_username],
+                                                                'password' => inputs[:default_password].to_s.empty? ? '' : inputs[:default_password]}}})
+        end
 
         model.save
         model
