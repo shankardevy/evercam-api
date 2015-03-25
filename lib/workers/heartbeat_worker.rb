f5335fa0833d75e379e0817a712aa3eca4841176
Miloš Mošić <mosic.milos@gmail.com>
2015-03-25 15:23:30 +0100

    Fixed digest auth for the heartbeat worker

--- lib/workers/heartbeat_worker.rb
+++ lib/workers/heartbeat_worker.rb
@@ -3,6 +3,7 @@ require 'faraday'
 require 'mini_magick'
 require 'faraday/digestauth'
 require 'dalli'
+require 'curb'
 require 'active_support'
 require 'active_support/core_ext'
 require 'concurrent/utilities'
@@ -43,8 +44,21 @@ module Evercam
         response = conn.get do |req|
           req.url camera.res_url('jpg')
         end
+        if response.status == 401
+          digest_response = Curl::Easy.new("#{camera.external_url}#{camera.res_url('jpg')}")
+          digest_response.http_auth_types = :digest
+          digest_response.username = camera.cam_username
+          digest_response.password = camera.cam_password
+          digest_response.perform
+
+          _http_response, *http_headers = digest_response.header_str.split(/[\r\n]+/).map(&:strip)
+          http_headers = Hash[http_headers.flat_map{ |s| s.scan(/^(\S+): (.+)/) }]
+
+          response = OpenStruct.new({'status' => digest_response.response_code, 'body' => digest_response.body, 'headers' => http_headers})
+        end
+
         if response.status == 200
-          if response.headers.fetch('content-type', '').start_with?('image')
+          if response.headers.fetch('Content-Type', '').start_with?('image')
             image = MiniMagick::Image.read(response.body)
 
             filepath = "#{camera.exid}/snapshots/#{instant.to_i}.jpg"
@@ -82,78 +96,82 @@ module Evercam
     end
 
     def perform(camera_exid)
-      Concurrent.timeout(30) do
-        logger.info("Started update for camera #{camera_exid}")
-        instant = Time.now
-        camera = Camera.by_exid(camera_exid)
-        return if camera.nil?
-        updates = { is_online: false, last_polled_at: instant }
+      if [
+        "carrollszoocam",
+        "gpocam",
+        "wayra-office",
+        "wayra-agora",
+        "wayrahikvision",
+        "zipyard-navan-foh",
+        "zipyard-ranelagh-foh",
+        "ndrc-main",
+        "ndrc-foodcam",
+        "gemcon-cathalbrugha",
+        "smartcity1",
+        "stephens-green",
+        "treacyconsulting1",
+        "treacyconsulting2",
+        "treacyconsulting3",
+        "dcctestdumping1",
+        "dcctestdumping2",
+        "beefcam1",
+        "beefcam2",
+        "beefcammobile",
+        "bennett"
+      ].include? camera_exid
+        Evercam::HeartbeatWorker.enqueue(camera_exid, camera_exid)
+      else
+        UniqueQueueWorker.enqueue_if_unique('heartbeat', Evercam::HeartbeatWorker, camera_exid)
+      end
+      begin
+        Concurrent.timeout(30) do
+          logger.info("Started update for camera #{camera_exid}")
+          instant = Time.now
+          camera = Camera.by_exid(camera_exid)
+          return if camera.nil?
+          updates = { is_online: false, last_polled_at: instant }
 
-        unless camera.external_url.nil?
-          updates = snap_request(camera, updates, instant)
-        end
-        if camera.is_online and not updates[:is_online]
-          # Try one more time, some cameras are dumb
-          updates = snap_request(camera, updates, instant)
-          unless updates[:is_online]
+          unless camera.external_url.nil?
+            updates = snap_request(camera, updates, instant)
+          end
+          if camera.is_online and not updates[:is_online]
+            # Try one more time, some cameras are dumb
+            updates = snap_request(camera, updates, instant)
+            unless updates[:is_online]
+              CameraActivity.create(
+                camera: camera,
+                access_token: nil,
+                action: 'offline',
+                done_at: Time.now,
+                ip: nil
+              )
+            end
+          end
+          if not camera.is_online and updates[:is_online]
             CameraActivity.create(
               camera: camera,
               access_token: nil,
-              action: 'offline',
+              action: 'online',
               done_at: Time.now,
               ip: nil
             )
           end
-        end
-        if not camera.is_online and updates[:is_online]
-          CameraActivity.create(
-            camera: camera,
-            access_token: nil,
-            action: 'online',
-            done_at: Time.now,
-            ip: nil
-          )
-        end
-        camera_is_online = camera.is_online
-        trigger_webhook(camera)
-        camera.update(updates)
-        cached_camera = Evercam::Services::dalli_cache.get(camera.exid)
-        cached_thumbnail_url = cached_camera.blank? ? '' : cached_camera.thumbnail_url
-        if camera_is_online == true || updates[:is_online] == true
-          if (camera_is_online != updates[:is_online]) ||
-              (thumbnail_token_time(cached_thumbnail_url) < thumbnail_token_time(camera.thumbnail_url) - 30.seconds)
-            Evercam::Services.dalli_cache.set(camera_exid, camera, 0)
-            CacheInvalidationWorker.enqueue(camera.exid)
+          camera_is_online = camera.is_online
+          trigger_webhook(camera)
+          camera.update(updates)
+          cached_camera = Evercam::Services::dalli_cache.get(camera.exid)
+          cached_thumbnail_url = cached_camera.blank? ? '' : cached_camera.thumbnail_url
+          if camera_is_online == true || updates[:is_online] == true
+            if (camera_is_online != updates[:is_online]) ||
+                (thumbnail_token_time(cached_thumbnail_url) < thumbnail_token_time(camera.thumbnail_url) - 30.seconds)
+              Evercam::Services.dalli_cache.set(camera_exid, camera, 0)
+              CacheInvalidationWorker.enqueue(camera.exid)
+            end
           end
+          logger.info("Update for camera #{camera.exid} finished. New status #{updates[:is_online]}")
         end
-        if [
-          "carrollszoocam",
-          "gpocam",
-          "wayra-office",
-          "wayra-agora",
-          "wayrahikvision",
-          "zipyard-navan-foh",
-          "zipyard-ranelagh-foh",
-          "ndrc-main",
-          "ndrc-foodcam",
-          "gemcon-cathalbrugha",
-          "smartcity1",
-          "stephens-green",
-          "treacyconsulting1",
-          "treacyconsulting2",
-          "treacyconsulting3",
-          "dcctestdumping1",
-          "dcctestdumping2",
-          "beefcam1",
-          "beefcam2",
-          "beefcammobile",
-          "bennett"
-        ].include? camera_exid
-          Evercam::HeartbeatWorker.enqueue(camera_exid, camera_exid)
-        else
-          UniqueQueueWorker.enqueue_if_unique('heartbeat', Evercam::HeartbeatWorker, camera.exid)
-        end
-        logger.info("Update for camera #{camera.exid} finished. New status #{updates[:is_online]}")
+      rescue Concurrent::TimeoutError
+        # timeout
       end
     end
 
