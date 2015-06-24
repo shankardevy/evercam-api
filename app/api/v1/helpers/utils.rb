8e783156c4b26883b2e2198332c0b70041192341
Miloš Mošić <mosic.milos@gmail.com>
2015-06-24 12:28:39 +0200

    Removed redundant begin blocks

--- app/api/v1/helpers/utils.rb
+++ app/api/v1/helpers/utils.rb
@@ -1,22 +1,18 @@
 module Evercam
   module Utils
     def self.is_num?(str)
-      begin
-        !!Integer(str)
-      rescue ArgumentError, TypeError
-        false
-      end
+      !!Integer(str)
+    rescue ArgumentError, TypeError
+      false
     end
   end
 end
 
 module JSON
   def self.is_json?(json)
-    begin
-      return false unless json.is_a?(String)
-      JSON.parse(json).all?
-    rescue JSON::ParserError
-      false
-    end
+    return false unless json.is_a?(String)
+    JSON.parse(json).all?
+  rescue JSON::ParserError
+    false
   end
 end
