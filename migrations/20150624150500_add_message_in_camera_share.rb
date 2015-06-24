f1487772d0b768942a0ecee326c45395a40ec365
Azhar Malik <azharmalik02@gmail.com>
2015-06-24 15:53:41 +0500

    Added migrate file to add message column in tables

---
+++ migrations/20150624150500_add_message_in_camera_share.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+
+  up do
+    alter_table(:camera_shares) do
+      add_column :message, Text
+    end
+  end
+
+  down do
+    alter_table(:camera_shares) do
+      drop_column :message
+    end
+  end
+
+end
---
+++ migrations/20150624150500_add_message_in_camera_share_requests.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+
+  up do
+    alter_table(:camera_share_requests) do
+      add_column :message, Text
+    end
+  end
+
+  down do
+    alter_table(:camera_share_requests) do
+      drop_column :message
+    end
+  end
+
+end
