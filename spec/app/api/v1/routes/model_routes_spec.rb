ab52000c7bd9fa1874ef5bd1a90361b77673bf39
shakeelanjum <shakeelmanjum@gmail.com>
2015-07-07 13:27:29 +0500

    Removed 'images' from models rspec keys

--- spec/app/api/v1/routes/model_routes_spec.rb
+++ spec/app/api/v1/routes/model_routes_spec.rb
@@ -26,7 +26,7 @@ describe 'API routes/models' do
       end
 
       it 'returns the model data' do
-        expect(json[0]).to have_keys("id", "name", "vendor_id", "default_username", "default_password", "jpg_url", "h264_url", "mjpg_url", "shape", "resolution", "official_url", "audio_url", "more_info", "poe", "wifi", "upnp", "ptz", "infrared", "varifocal", "sd_card", "audio_io", "onvif", "psia", "discontinued", "icon_image", "thumbnail_image", "original_image", "defaults", "images")
+        expect(json[0]).to have_keys("id", "name", "vendor_id", "default_username", "default_password", "jpg_url", "h264_url", "mjpg_url", "shape", "resolution", "official_url", "audio_url", "more_info", "poe", "wifi", "upnp", "ptz", "infrared", "varifocal", "sd_card", "audio_io", "onvif", "psia", "discontinued", "icon_image", "thumbnail_image", "original_image", "defaults")
       end
 
       it 'has pagination' do
@@ -55,7 +55,7 @@ describe 'API routes/models' do
       end
 
       it 'returns the model data' do
-        expect(json[0]).to have_keys("id", "name", "vendor_id", "default_username", "default_password", "jpg_url", "h264_url", "mjpg_url", "shape", "resolution", "official_url", "audio_url", "more_info", "poe", "wifi", "upnp", "ptz", "infrared", "varifocal", "sd_card", "audio_io", "onvif", "psia", "discontinued", "icon_image", "thumbnail_image", "original_image", "defaults", "images")
+        expect(json[0]).to have_keys("id", "name", "vendor_id", "default_username", "default_password", "jpg_url", "h264_url", "mjpg_url", "shape", "resolution", "official_url", "audio_url", "more_info", "poe", "wifi", "upnp", "ptz", "infrared", "varifocal", "sd_card", "audio_io", "onvif", "psia", "discontinued", "icon_image", "thumbnail_image", "original_image", "defaults")
       end
 
       it 'only returns supported models' do
