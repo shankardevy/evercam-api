eee065b0b58a7e11e90df982b80af07b6bd18e29
Azhar Malik <azharmalik02@gmail.com>
2015-07-07 13:26:49 +0500

    Added clip delete endpoint

--- app/api/v1/routes/archive_routes.rb
+++ app/api/v1/routes/archive_routes.rb
@@ -13,7 +13,7 @@ module Evercam
       #-------------------------------------------------------------------------
       # GET /v1/cameras/:id/archives
       #-------------------------------------------------------------------------
-      desc 'Returns available archives for the camera',{
+      desc 'Returns available clips for the camera',{
         entity: Evercam::Presenters::Archive
       }
       params do
@@ -35,7 +35,7 @@ module Evercam
       #-------------------------------------------------------------------------
       # GET /v1/cameras/:id/archives/archive_id
       #-------------------------------------------------------------------------
-      desc 'Returns available archives for the camera',{
+      desc 'Returns all data for a given clip',{
                                                         entity: Evercam::Presenters::Archive
                                                       }
       params do
@@ -52,16 +52,14 @@ module Evercam
           end
         end
         archive = Archive.where(exid: params[:archive_id])
-        if archive.count == 0
-          raise NotFoundError.new("The '#{params[:archive_id]}' clip does not exist.")
-        end
+        raise NotFoundError.new("The '#{params[:archive_id]}' clip does not exist.") if archive.count == 0
         present Array(archive), with: Presenters::Archive
       end
 
       #-------------------------------------------------------------------------
       # POST /v1/cameras/:id/archives
       #-------------------------------------------------------------------------
-      desc 'Returns available archives for the camera',{
+      desc 'Create a new clip',{
         entity: Evercam::Presenters::Archive
       }
       params do
@@ -88,6 +86,24 @@ module Evercam
           raise OutcomeError, outcome.to_json
         end
         present Array(outcome.result), with: Presenters::Archive
+      end
+
+      #-------------------------------------------------------------------------
+      # DELETE /v1/cameras/:id/archives/archive_id
+      #-------------------------------------------------------------------------
+      desc 'Delete clip from archives'
+      params do
+        requires :id, type: String, desc: 'The unique identifier for the camera.'
+        requires :archive_id, type: String, desc: 'The unique identifier for the clip.'
+      end
+      delete '/:id/archives/archive_id' do
+        camera = Camera.by_exid!(params[:id])
+        raise NotFoundError.new unless camera
+        rights = requester_rights_for(camera)
+        raise AuthorizationError.new if !rights.allow?(AccessRight::DELETE)
+        archive = ::Archive.where(exid: params[:archive_id])
+        raise NotFoundError.new("The '#{params[:archive_id]}' clip does not exist.") if archive.count == 0
+        archive.destroy
       end
     end
   end
