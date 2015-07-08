4c776ec8c8d7fc6017e317d5afffad923125f0aa
Azhar Malik <azharmalik02@gmail.com>
2015-07-08 11:58:04 +0500

    Added migration file for archive table

---
+++ migrations/20150708110000_create_archive_table.rb
@@ -0,0 +1,21 @@
+Sequel.migration do
+  up do
+    create_table(:archives) do
+      primary_key :id
+      foreign_key :camera_id, :cameras, on_delete: :cascade, null: false
+      foreign_key :requested_by, :users, on_delete: :cascade, null: false
+      column :exid, :text, null: false
+      column :title, :text, null: false
+      column :from_date, :timestamptz, null: false
+      column :to_date, :timestamptz, null: false
+      column :status, :integer, null: false
+      column :created_at, :timestamptz, null: false
+      column :embed_time, :boolean, null: false
+      column :public, :boolean, null: false
+    end
+  end
+
+  down do
+    drop_table(:archives)
+  end
+end
